849c7683b9e89d495f1e9e5c3c464f2862abd096
Paweł Wilk <siefca@gnu.org>
2009-02-02 02:04:13 +0100

    Changing relations between classes, renaming classes, major code reorganization in progress.

--- readline-utf-8-fix/fix-readline.rb
+++ readline-utf-8-fix/fix-readline.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env ruby
 
-# fix yields in choicesdialog!!!
-
 class Pathnames < Array
 
   def initialize(size=0, obj=nil)
@@ -120,11 +118,11 @@ class ChoiceDialogs
     return choice
   end
 
-  def display_list
+  def display_list(&block)
     return nil if @objects.nil?
     if @objects.size > 1
       # more than one
-      choice    = ask_for_choice { |t| yield(t) }
+      choice    = ask_for_choice &block
       selected  = @objects[choice.to_i-1]
     elsif @objects.size == 1
       # exactly one found
@@ -138,54 +136,155 @@ class ChoiceDialogs
 
 end
 
-# This class keeps information describing library:
-# found version number, required version number,
-# name, includes directory, library directory and filenames.
-# It's methods can seek for a proper version of library
-# and header files in Mac OS X.
+class RequiredCommand
 
-class LibraryPath
-  attr_reader :include_path, :lib_path, :lib_name,
-  :inc_paths, :lib_paths, :lib_version,
-  :lib_dir_path, :include_dir_path,
-  :inc_filename, :lib_filename, :lib_req_ver
+  attr_reader :inst_command, :req_commands, :tool_path
+              :cmd_missing, :tool_name, :tool_commands, :bin_search_paths
 
-  def readliblink(filename)
-    return nil if (filename.nil? || filename.empty?)
-    dirpart = File.dirname filename
-    libpath = UnixTool.follow_link(filename)
-    return nil if (libpath.nil? || libpath.empty?)
+  # _tool\_name_ - name of a tool,
+  # _tool\_cmd_ - command covering tool's existence,
+  # _req\_commands_ - colon-separated list of commands required to install,
+  # _inst\_command_ - command used to install the tool,
+  # _bin_search\_paths - colon-separated list of paths where commands are looked in,
+  # (defaults to content of +PATH+ environment variable if not given)
+  def initialize(tool_name, tool_command=nil, tool_req_commands=nil,
+                 tool_inst_command=nil, bin_search_paths=nil)
 
-    libpath_full      = File.expand_path(libpath, dirpart)
-    libpath_basename  = File.basename(libpath)
-    return nil if (libpath_basename.nil? || libpath_basename.empty?)
+    if tool_name.respond_to?(:key?)
+      args, tool_name = tool_name, nil
+    end
 
-    ary = libpath_basename.split '.'
-    return nil if (ary.nil? || ary.size < 2)
-    lib_version = ary[1].to_i
-    return libpath_full, lib_version
+    @tool_name          = get_arg(args, :tool_name,         tool_name)
+    @tool_command       = get_arg(args, :tool_commands,     tool_commands)
+    @tool_inst_command  = get_arg(args, :inst_command,      inst_command)
+    @tool_req_commands  = get_arg(args, :req_commands,      req_commands)
+    @bin_search_paths   = get_arg(args, :bin_search_paths,  bin_search_paths)
+    @bin_search_paths   = ENV['PATH'] if @bin_search_paths.nil?
+
+    @cmd_missing  = Array.new
+    if !@tool_req_commands.nil?
+      @tool_req_commands.split(':').each do |cmd|
+        @cmd_missing << cmd if UnixTool.whereis(cmd, @bin_search_paths).nil?
+      end
+    end
+
+    @tool_path = @tool_command.nil? ? nil : UnixTool.whereis(@tool_command, @bin_search_paths)
   end
 
   def get_arg(hash, name, alternative)
     return (hash.respond_to?(:key?) && hash.key?(name)) ? hash[name] : alternative
   end
 
-  def initialize(inc_paths, lib_paths=nil, lib_req_ver=nil, lib_name=nil)
-    if !inc_paths.nil? && inc_paths.respond_to?(:key?)
-      args, inc_paths = inc_paths, nil
+  # Returns an array containing required installation commands that are missing, or +nil+
+  def missing_install_commands
+    @cmd_missing.nil? ? nil : @cmd_missing.dup
+  end
+
+  # Returns +true+ if tool command has been found.
+  def tool_found?
+    !@tool_path.nil? && !@tool_path.empty? 
+  end
+
+  def needs_install?
+    not tool_found?
+  end
+  
+  def installation_possible?
+    @cmd_missing.empty? && !@tool_inst_command.nil?
+  end
+
+  # Returns +true+ if it is possible to use installation tool.
+  def should_be_installed?
+    needs_install? && installation_possible?
+  end
+
+  # Runs installation command(s) assgined to a tool.
+  def run_install
+    installation_possible? ? system(@tool_inst_command) : nil
+  end
+
+  # Runs installation command(s) assgined to a tool.
+  def install_if_missing
+    should_be_installed? ? system(@tool_inst_command) : nil
+  end
+
+end
+
+# This class extends class RequiredCommand by adding
+# methods and data structures handling common libraries.
+#
+# This class reads information about library.
+# It tracks its version number, pathname of header file,
+# pathname of library file, 
+
+class RequiredLibrary < RequiredCommand
+  attr_reader :include_path, :lib_path, :lib_name,
+  :inc_search_paths, :lib_search_paths, :lib_version,
+  :lib_dir_path, :include_dir_path,
+  :inc_filename, :lib_filename, :lib_req_ver
+
+# "fink", "readline", 5,
+# "readline.h", "libreadline.dylib",
+# "/usr/bin", "/usr/lib", "/usr/include",
+#  "fink:sudo", "fink install readline4"
+
+# tool_inst_command has changed meaning!!! it's now about the library, not tool
+
+  def initialize(tool_name, lib_name, lib_req_ver=nil,
+                 inc_filename=nil, lib_filename=nil,
+                 bin_search_paths=nil, lib_search_paths=nil, inc_search_paths=nil,
+                 tool_req_commands=nil, tool_inst_command=nil)
+
+    if tool_name.respond_to?(:key?)
+      args, tool_name = tool_name, nil
+      super(args, tool_command, tool_req_commands, tool_inst_command, bin_search_paths)
+    else
+      super(tool_name, tool_command, tool_req_commands, tool_inst_command, bin_search_paths)
     end
 
     @inc_paths    = get_arg(args, :inc_paths,     inc_paths)
-    @lib_paths    = get_arg(args, :lib_paths,     lib_paths)
+    #new: @search_paths @lib_paths    = get_arg(args, :lib_paths,     lib_paths)
     @lib_name     = get_arg(args, :lib_name,      lib_name)
     @inc_filename = get_arg(args, :inc_filename,  inc_filename)
     @lib_filename = get_arg(args, :lib_filename,  lib_filename)
     @lib_req_ver  = get_arg(args, :lib_req_ver,   lib_req_ver)
 
-    @lib_path, @lib_version = @lib_paths.nil? ? nil : readliblink(UnixTool.whereis(@lib_filename, @lib_paths))
+    if not @lib_search_paths.nil?
+      try_filenames = [ @lib_filename, @lib_filename.sub(/^lib/,''), "lib#{@lib_filename}" ]
+      matching_filename = nil
+      try_filenames.each do |lib_filename|
+        @lib_filename,
+        @lib_path,
+        @lib_version = readliblink(lib_filename, @lib_search_paths)
+        break if not @lib_path.nil?
+      end
+      @lib_version = nil if @lib_path.nil?
+    else
+      @lib_path = @lib_version = nil
+    end
+    
     @include_path     = @inc_paths.nil?     ? nil : UnixTool.whereis(@inc_filename, @inc_paths)
     @lib_dir_path     = @lib_path.nil?      ? nil : File.dirname(@lib_path)
     @include_dir_path = @include_path.nil?  ? nil : File.dirname(@include_path)
+  end
+
+  def readliblink(filename)
+    nil_ret = [ filename, nil, nil ]
+    return nil_ret if filename.nil?
+    filename = UnixTool.whereis(filename, @lib_search_paths)
+    return nil_ret if (filename.nil? || filename.empty?)
+    dirpart = File.dirname filename
+    libpath = UnixTool.follow_link(filename)
+    return nil_ret if (libpath.nil? || libpath.empty?)
+
+    libpath_full      = File.expand_path(libpath, dirpart)
+    libpath_basename  = File.basename(libpath)
+    return nil_ret if (libpath_basename.nil? || libpath_basename.empty?)
+
+    ary = libpath_basename.split '.'
+    return nil_ret if (ary.nil? || ary.size < 2)
+    lib_version = ary[1].to_i
+    return filename, libpath_full, lib_version
   end
 
   # Returns +true+ if major library version equals required.
@@ -237,67 +336,6 @@ class LibraryPath
 
 end
 
-# This class adds tool-related information and actions
-# to the class LibraryPath
-
-class Tool < LibraryPath
-
-  attr_reader :inst_command, :req_commands, :cmd_missing, :tool_name
-
-  # Creates new +Tool+ object containing paths for the library
-  # which will initialize fields required by LibraryPath superclass
-  # and information about installation tool, which may manage the
-  # library (package manager).
-  #
-  # _inst\_command_ whould contain a path
-  # to library installation shell commands, _req\_commands_
-  # should contain names of shell commands separated by colons
-  # which are required for installation to succeed,
-  # and _tool\_name_ should contain name of a tool.
-  #
-  # You can also pass all agruments as a hash. Names for keys
-  # should be the same as names of arguments mentioned before.
-
-  def initialize(inst_command, req_commands=nil, tool_name=nil,
-    inc_paths=nil, lib_paths=nil, lib_req_ver=nil, lib_name=nil)
-
-    if !inst_command.nil? && inst_command.respond_to?(:key?)
-      args, inst_command = inst_command, nil
-      args[:inc_paths] = get_arg(args, :inc_paths, inc_paths)
-      super(args, lib_paths, lib_name)
-    else
-      super(inc_paths, lib_paths, lib_name)
-    end
-
-    @tool_name    = get_arg(args, :tool_name,    tool_name)
-    @inst_command = get_arg(args, :inst_command,  inst_command)
-    @req_commands = get_arg(args, :req_commands,  req_commands)
-
-    @cmd_missing  = Array.new
-    if !@req_commands.nil?
-      @req_commands.split(':').each do |cmd|
-        @cmd_missing << cmd if UnixTool.whereis(cmd).nil?
-      end
-    end
-  end
-
-  # Returns an array containing required commands that are missing or +nil+
-  def missing_commands
-    @cmd_missing.nil? ? nil : @cmd_missing.dup
-  end
-
-  # Returns true if it is possible to use installation tool.
-  def usable?
-    @cmd_missing.empty? && !@inst_command.nil?
-  end
-
-  # Runs installation command(s) assgined to a tool.
-  def run_install
-    usable? ? system(@inst_command) : nil
-  end
-
-end
-
 # This class keeps tools.
 
 class Tools <Array
@@ -322,8 +360,8 @@ fink  = Tool.new(
 :tool_name    =>  "fink",
 :lib_name     =>  "readline",
 :lib_req_ver  =>  (5..6),
-:req_commands =>  "fink:curl:git",
-:inst_command =>  "sudo port install readline +universal",
+:req_commands =>  "fink",
+:inst_command =>  "fink install readline5",
 :lib_paths    =>  "/sw/lib:/sw/local/lib:/sw/usr/local/lib:/sw/usr/lib",
 :inc_paths    =>  "/sw/include/readline:/sw/local/include/readline:"  +
                   "/sw/usr/local/include/readline:/sw/include:"       +
@@ -336,8 +374,8 @@ port  = Tool.new(
 :tool_name    =>  "port",
 :lib_name     =>  "readline",
 :lib_req_ver  =>  (5..6),
-:req_commands =>  "port:curl:git",
-:inst_command =>  "fink install readline5",
+:req_commands =>  "port:sudo",
+:inst_command =>  "sudo port install readline +universal",
 :lib_paths    =>  "/opt/local/lib:/opt/usr/local/lib:/opt/lib:/opt/usr/lib",
 :inc_paths    =>  "/opt/local/include/readline:/opt/usr/local/include/readline:"    +
                   "/opt/include/readline:/opt/local/include:/opt/usr/local/inlude:" +
@@ -350,7 +388,7 @@ shell = Tool.new(
 :tool_name    =>  "shell script",
 :lib_name     =>  "readline",
 :lib_req_ver  =>  (5..6),
-:req_commands =>  "gcc:rm:mv:make:curl:tar:gzip:sudo:gpg:git:pipi",
+:req_commands =>  "gcc:rm:mv:make:curl:tar:gzip:sudo:gpg:git",
 
 :inst_command =>  "rm -rf ./build && ./mkdir build && cd ./build && "                               +
                   "curl --retry 2 -C - -O http://ftp.gnu.org/gnu/readline/readline-5.2.tar.gz && "  +
@@ -373,7 +411,7 @@ native = Tool.new(
 :tool_name    =>  "Mac OS X",
 :lib_name     =>  "readline",
 :lib_req_ver  =>  (5..6),
-:lib_paths    =>  "/usr/lib:/lib:/usr/lib/readline:/usr/lib/readline/lib",
+:lib_paths    =>  "/usr/lib:/lib:/usr/lib/readline:/usr/lib/readline/lib:/System/Library/Frameworks:/Library/Frameworks",
 :inc_paths    =>  "/usr/include/readline:/usr/include",
 :inc_filename =>  "readline.h",
 :lib_filename =>  "libreadline.dylib"
@@ -381,18 +419,44 @@ native = Tool.new(
 
 common_build_tools = Tool.new(
 :tool_name    =>  "prerequisites",
-:req_commands =>  "gcc:rm:mv:make:tar:gzip:sudo:gpg:curl",
+:req_commands =>  "gpg:curl:sdfsdf:upa:siefca:lsls",
 :inst_command =>  "echo"
 )
+
+build_tools_shell = Tools.new [
+  Tool.new("Tape ARchiver",     "tar"),
+  Tool.new("GNU make",          "make"),
+  Tool.new("GNU C Compiler",    "gcc"),
+  Tool.new("GNU Privacy Guard", "gpg"),
+  Tool.new("CURL", "curl")
+]
+
+build_tools_fink = Tools.new [
+  Tool.new("Tape ARchiver",   "tar",  "fink", "fink install tar"),
+  Tool.new("GNU make",        "make", "fink", "fink install make"),
+  Tool.new("GNU C Compiler",  "gcc",  "fink", "fink install gcc4"),
+]
+
+build_tools_port = Tools.new [
+  Tool.new("Tape ARchiver",   "tar",  "port:sudo", "sudo port install tar"),
+  Tool.new("GNU make",        "make", "port:sudo", "sudo port install make"),
+  Tool.new("GNU C Compiler",  "gcc",  "port:sudo", "sudo port install gcc43"),
+]
 
 ## Check for tools that we need to build the library
 
 puts "\nPHASE 0: Looking for common tools.\n\n"
 
-if not common_build_tools.usable?
-  STDERR.puts "Cannot find common tools required to apply this fix."
-  exit 1
-end
+ENV['PATH'] += ":/sw/bin:/opt/local/bin:/usr/local/bin"
+
+#if not common_build_tools.usable?
+#  STDERR.puts "Mission aborted due to unmet dependencies."
+#  STDERR.puts "Tools required to apply this fix that cannot be found in PATH:"
+#  common_build_tools.missing_commands.each { |cmd| STDERR.puts " => #{cmd}" }
+#  exit 1
+#else
+#  puts "Found all commands required to apply this fix."
+#end
 
 ## seek after completed paths (includes and library)
 
@@ -416,13 +480,11 @@ end
 if library.nil?
   needs_install = true
   puts "Cannot find any proper version of readline library."
-  puts "I'll try to assist you in building or installing one.\n\n"
-
+  puts "I'll try to assist you in building or installing one.\n\n"  
+  
   library = choose_installer.display_list do |installer|
     "Use #{installer.tool_name} (installs #{installer.lib_name} in #{installer.lib_dir_preferred})"
   end
-  
-  puts "I will use #{library.tool_name} to install readline library." if not library.nil?
 else
   puts "I've found version #{library.lib_version} of #{library.lib_name} library in #{library.lib_dir_path}"
 end
@@ -439,11 +501,20 @@ if library.nil?
   STDERR.puts
 
   exit 1
+elsif needs_install
+  build_tools = Tools.new
+  puts "I will use #{library.tool_name} to install readline library." if not library.nil?
+  if (library == fink || library == port)
+    build_tools.add << gcc
+    
+  end
 end
 
-## Check version
+## Check Ruby version
 
-## Fetch sources
+## Fetch Ruby readline code
+
+## Fetch readline sources
 
 ## Generate patch
 
